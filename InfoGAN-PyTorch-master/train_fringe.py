287bd6117682012eba42b8c6c15d077f7cfa64f5
Suraj Menon <sjmickiemouse@gmail.com>
2022-03-05 13:01:40 -0800

    Idiot, need to save it in the main loop

--- InfoGAN-PyTorch-master/train_fringe.py
+++ InfoGAN-PyTorch-master/train_fringe.py
@@ -328,8 +328,10 @@ for epoch in range(params['num_epochs']):
         torch.save({
             'netG' : netG.state_dict(),
             'discriminator' : discriminator.state_dict(),
+            'classifier' : classifier.state_dict(),
             'netD' : netD.state_dict(),
             'netQ' : netQ.state_dict(),
+            'netC' : netC.state_dict(),
             'optimD' : optimD.state_dict(),
             'optimG' : optimG.state_dict(),
             'params' : params
