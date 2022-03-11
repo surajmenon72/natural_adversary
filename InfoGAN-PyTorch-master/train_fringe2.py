cb869ee5dccef4859d07dadbe9c6e01e6f1aa036
Suraj Menon <sjmickiemouse@gmail.com>
2022-03-11 11:46:17 -0800

    Adding new training file for Adversarial Classifier.

---
+++ InfoGAN-PyTorch-master/train_fringe2.py
@@ -0,0 +1,407 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+import random
+
+from models.mnist_model_exp import Generator, Discriminator, DHead, Classifier, CHead, SHead, QHead
+from dataloader import get_data
+from utils import *
+from config import params
+
+if(params['dataset'] == 'MNIST'):
+    from models.mnist_model_exp import Generator, Discriminator, DHead, Classifier, CHead, SHead, QHead
+elif(params['dataset'] == 'SVHN'):
+    from models.svhn_model import Generator, Discriminator, DHead, QHead
+elif(params['dataset'] == 'CelebA'):
+    from models.celeba_model import Generator, Discriminator, DHead, QHead
+elif(params['dataset'] == 'FashionMNIST'):
+    from models.mnist_model import Generator, Discriminator, DHead, QHead
+
+# Set random seed for reproducibility.
+seed = 1123
+random.seed(seed)
+torch.manual_seed(seed)
+print("Random Seed: ", seed)
+
+# Use GPU if available.
+device = torch.device("cuda:0" if(torch.cuda.is_available()) else "cpu")
+print(device, " will be used.\n")
+
+load_model = False
+if (load_model):
+    load_path = './checkpoints/model_load'
+    state_dict = torch.load(load_path, map_location=device)
+
+dataloader = get_data(params['dataset'], params['batch_size'])
+
+# Set appropriate hyperparameters depending on the dataset used.
+# The values given in the InfoGAN paper are used.
+# num_z : dimension of incompressible noise.
+# num_dis_c : number of discrete latent code used.
+# dis_c_dim : dimension of discrete latent code.
+# num_con_c : number of continuous latent code used.
+if(params['dataset'] == 'MNIST'):
+    params['num_z'] = 62
+    params['num_dis_c'] = 1
+    params['dis_c_dim'] = 10
+    params['num_con_c'] = 10 #continuous variable allocated for each class
+elif(params['dataset'] == 'SVHN'):
+    params['num_z'] = 124
+    params['num_dis_c'] = 4
+    params['dis_c_dim'] = 10
+    params['num_con_c'] = 4
+elif(params['dataset'] == 'CelebA'):
+    params['num_z'] = 128
+    params['num_dis_c'] = 10
+    params['dis_c_dim'] = 10
+    params['num_con_c'] = 0
+elif(params['dataset'] == 'FashionMNIST'):
+    params['num_z'] = 62
+    params['num_dis_c'] = 1
+    params['dis_c_dim'] = 10
+    params['num_con_c'] = 2
+
+# Plot the training images.
+sample_batch = next(iter(dataloader))
+plt.figure(figsize=(10, 10))
+plt.axis("off")
+plt.imshow(np.transpose(vutils.make_grid(
+    sample_batch[0].to(device)[ : 100], nrow=10, padding=2, normalize=True).cpu(), (1, 2, 0)))
+plt.savefig('Training Images {}'.format(params['dataset']))
+plt.close('all')
+
+# Initialise the network.
+netG = Generator().to(device)
+netG.apply(weights_init)
+print(netG)
+
+discriminator = Discriminator().to(device)
+discriminator.apply(weights_init)
+print(discriminator)
+
+netD = DHead().to(device)
+netD.apply(weights_init)
+print(netD)
+
+classifier = Classifier().to(device)
+classifier.apply(weights_init)
+print(classifier)
+
+netC = CHead().to(device)
+netC.apply(weights_init)
+print(netC)
+
+# netS = SHead().to(device)
+# netS.apply(weights_init)
+# print(netS)
+
+netQ = QHead().to(device)
+netQ.apply(weights_init)
+print(netQ)
+
+if (load_model):
+    netG.load_state_dict(state_dict['netG'])
+    discriminator.load_state_dict(state_dict['discriminator'])
+    netD.load_state_dict(state_dict['netD'])
+    classifier.load_state_dict(state_dict['classifier'])
+    netC.load_state_dict(state_dict['netC'])
+    netQ.load_state_dict(state_dict['netQ'])
+
+
+# Loss for discrimination between real and fake images.
+criterionD = nn.BCELoss()
+# Loss for classifier
+criterionC = nn.CrossEntropyLoss()
+# Loss for split between identity and controversy, just use CrossEntropy
+criterionS = nn.KLDivLoss()
+# Loss for discrete latent code.
+criterionQ_dis = nn.CrossEntropyLoss()
+# Loss for continuous latent code.
+criterionQ_con = NormalNLLLoss()
+
+# Adam optimiser is used.
+optimD = optim.Adam([{'params': discriminator.parameters()}, {'params': netD.parameters()}, {'params': classifier.parameters()}, {'params': netC.parameters()}], lr=params['learning_rate'], betas=(params['beta1'], params['beta2']))
+optimG = optim.Adam([{'params': netG.parameters()}, {'params': netQ.parameters()}], lr=params['learning_rate'], betas=(params['beta1'], params['beta2']))
+
+# Fixed Noise
+z = torch.randn(100, params['num_z'], 1, 1, device=device)
+fixed_noise = z
+if(params['num_dis_c'] != 0):
+    idx = np.arange(params['dis_c_dim']).repeat(10)
+    dis_c = torch.zeros(100, params['num_dis_c'], params['dis_c_dim'], device=device)
+    for i in range(params['num_dis_c']):
+        dis_c[torch.arange(0, 100), i, idx] = 1.0
+
+    dis_c = dis_c.view(100, -1, 1, 1)
+
+    fixed_noise = torch.cat((fixed_noise, dis_c), dim=1)
+
+if(params['num_con_c'] != 0):
+    con_c = torch.rand(100, params['num_con_c'], 1, 1, device=device) * 2 - 1
+    fixed_noise = torch.cat((fixed_noise, con_c), dim=1)
+
+real_label = 1
+fake_label = 0
+
+# List variables to store results pf training.
+img_list = []
+G_losses = []
+D_losses = []
+C_losses = []
+S_losses = []
+
+print("-"*25)
+print("Starting Training Loop...\n")
+print('Epochs: %d\nDataset: {}\nBatch Size: %d\nLength of Data Loader: %d'.format(params['dataset']) % (params['num_epochs'], params['batch_size'], len(dataloader)))
+print("-"*25)
+
+start_time = time.time()
+iters = 0
+
+#Realness vs. Classification Hyperparams
+alpha = 1
+beta = 1
+
+for epoch in range(params['num_epochs']):
+    epoch_start_time = time.time()
+
+    for i, (data, true_label) in enumerate(dataloader, 0):
+        # print ('Batch')
+        # print (i)
+        # Get batch size
+        b_size = data.size(0)
+        # Transfer data tensor to GPU/CPU (device)
+        real_data = data.to(device)
+        true_label_g = true_label.to(device)
+
+
+        #get labels, targets
+        true_labels, targets = get_targets(true_label_g, params['dis_c_dim'], device)
+
+        #get noise sample
+        noise, idx, c_nums = noise_sample_target(params['num_dis_c'], params['dis_c_dim'], params['num_con_c'], params['num_z'], b_size, device, targets)
+
+        # Updating discriminator and DHead
+        discriminator.train()
+        netD.train()
+        classifier.train()
+        netC.train()
+        optimD.zero_grad()
+        # Real data
+        label = torch.full((b_size, ), real_label, device=device)
+        output1 = discriminator(real_data)
+        probs_real = netD(output1).view(-1)
+        label = label.to(torch.float32)
+        loss_real = criterionD(probs_real, label)
+        loss_real = loss_real*alpha
+        # Calculate gradients.
+        loss_real.backward()
+
+        #Train classifier
+        # output_c = classifier(real_data)
+        # #probs_c = netS(output_c)
+        # probs_c = netC(output_c)
+        # probs_c = torch.squeeze(probs_c)
+        # #true_labels = true_labels.to(torch.float32)
+        # loss_c = criterionC(probs_c, true_label_g)
+        # loss_c = loss_c*beta
+        # #Calculate gradients
+        # loss_c.backward()
+        loss_c = torch.zeros(1)
+
+        # Fake data
+        label.fill_(fake_label)
+        #noise, idx = noise_sample(params['num_dis_c'], params['dis_c_dim'], params['num_con_c'], params['num_z'], b_size, device)
+        #noise, idx, c_nums = noise_sample_target(params['num_dis_c'], params['dis_c_dim'], params['num_con_c'], params['num_z'], b_size, device, targets)
+        fake_data = netG(noise)
+        output2 = discriminator(fake_data.detach())
+        probs_fake = netD(output2).view(-1)
+        loss_fake = criterionD(probs_fake, label)
+        loss_fake = loss_fake*alpha
+        # Calculate gradients.
+        loss_fake.backward()
+
+        #Entropy train Classifier (Adversarial or Cooperative)
+        fake_data = netG(noise)
+        output_e = classifier(fake_data)
+        probs_e = netC(output_e)
+        probs_e = F.softmax(probs_e, dim=1)
+        probs_e = torch.squeeze(probs_e)
+        entropy_e = calc_entropy(probs_e)
+        loss_e = torch.mean(entropy_e, dim=0)
+        #Calculate gradients.
+        loss_e.backward()
+
+        # Net Loss for the discriminator and classifier
+        D_loss = loss_real + loss_fake
+
+        # Update parameters
+        optimD.step()
+
+        # Updating Generator and QHead
+        netG.train()
+        netQ.train()
+        optimG.zero_grad()
+
+        #Split loss
+        # split_labels = get_split_labels(true_label_g, targets, c_nums, params['dis_c_dim'], device)
+        # fake_data = netG(noise)
+        # output_s = classifier(fake_data)
+        # #probs_split = netS(output_s)
+        # probs_split = netC(output_s)
+        # probs_split = F.log_softmax(probs_split, dim=1)
+        # probs_split = torch.squeeze(probs_split) #TODO: consider if there are extra channels 
+        # loss_split = criterionS(probs_split, split_labels)
+        # loss_split = loss_split*beta
+        # # Calculate gradients
+        # loss_split.backward()
+        # loss_split = torch.zeros(1)
+
+        #If we want classifier to force an output
+        # fake_labels = torch.zeros(true_label_g.shape[0], device=device)
+        # fake_labels = fake_labels.to(torch.int64)
+        # fake_data = netG(noise)
+        # output_s = classifier(fake_data)
+        # probs_split = netS(output_s)
+        # probs_split = torch.squeeze(probs_split)
+        # loss_split = criterionC(probs_split, fake_labels)
+        # loss_split.backward()
+
+        #Entropy Loss
+        fake_data = netG(noise)
+        output_s = classifier(fake_data)
+        probs_s = netC(output_s)
+        probs_s = F.softmax(probs_s, dim=1)
+        probs_s = torch.squeeze(probs_s)
+        entropy_s = calc_entropy(probs_s)
+        loss_s = -(torch.mean(entropy_s, dim=0))
+        #Calculate gradients
+        loss_s.backward()
+
+        # Fake data treated as real.
+        fake_data = netG(noise)
+        output = discriminator(fake_data)
+        label.fill_(real_label)
+        probs_fake = netD(output).view(-1)
+        gen_loss = criterionD(probs_fake, label)
+
+        q_logits, q_mu, q_var = netQ(output)
+        target = torch.LongTensor(idx).to(device)
+        # Calculating loss for discrete latent code.
+        dis_loss = 0
+        for j in range(params['num_dis_c']):
+            dis_loss += criterionQ_dis(q_logits[:, j*10 : j*10 + 10], target[j])
+
+        # Calculating loss for continuous latent code.
+        con_loss = 0
+        if (params['num_con_c'] != 0):
+            con_loss = criterionQ_con(noise[:, params['num_z']+ params['num_dis_c']*params['dis_c_dim'] : ].view(-1, params['num_con_c']), q_mu, q_var)*0.1
+
+        #Net loss for classifier
+        C_loss = loss_c
+        #Loss for Split
+        #S_loss = loss_split
+        S_loss = loss_s
+        # Net loss for generator.
+        G_loss = gen_loss + dis_loss + con_loss
+        G_loss = G_loss*alpha
+        # Calculate gradients.
+        G_loss.backward()
+        # Update parameters.
+        optimG.step()
+
+        # Check progress of training.
+        if i != 0 and i%100 == 0:
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tLoss_C: %.4f\tLoss_S: %.4f'
+                  % (epoch+1, params['num_epochs'], i, len(dataloader), 
+                    D_loss.item(), G_loss.item(), C_loss.item(), S_loss.item()))
+
+        # Save the losses for plotting.
+        G_losses.append(G_loss.item())
+        D_losses.append(D_loss.item())
+        C_losses.append(C_loss.item())
+        S_losses.append(S_loss.item())
+
+        iters += 1
+
+    epoch_time = time.time() - epoch_start_time
+    print("Time taken for Epoch %d: %.2fs" %(epoch + 1, epoch_time))
+    # Generate image after each epoch to check performance of the generator. Used for creating animated gif later.
+    with torch.no_grad():
+        gen_data = netG(fixed_noise).detach().cpu()
+    img_list.append(vutils.make_grid(gen_data, nrow=10, padding=2, normalize=True))
+
+    # Generate image to check performance of generator.
+    if((epoch+1) == 1 or (epoch+1) == params['num_epochs']/2):
+        with torch.no_grad():
+            gen_data = netG(fixed_noise).detach().cpu()
+        plt.figure(figsize=(10, 10))
+        plt.axis("off")
+        plt.imshow(np.transpose(vutils.make_grid(gen_data, nrow=10, padding=2, normalize=True), (1,2,0)))
+        plt.savefig("Epoch_%d {}".format(params['dataset']) %(epoch+1))
+        plt.close('all')
+
+    # Save network weights.
+    if (epoch+1) % params['save_epoch'] == 0:
+        torch.save({
+            'netG' : netG.state_dict(),
+            'discriminator' : discriminator.state_dict(),
+            'classifier' : classifier.state_dict(),
+            'netD' : netD.state_dict(),
+            'netQ' : netQ.state_dict(),
+            'netC' : netC.state_dict(),
+            'optimD' : optimD.state_dict(),
+            'optimG' : optimG.state_dict(),
+            'params' : params
+            }, 'checkpoint/model_fringe_epoch_%d_{}'.format(params['dataset']) %(epoch+1))
+
+training_time = time.time() - start_time
+print("-"*50)
+print('Training finished!\nTotal Time for Training: %.2fm' %(training_time / 60))
+print("-"*50)
+
+# Generate image to check performance of trained generator.
+with torch.no_grad():
+    gen_data = netG(fixed_noise).detach().cpu()
+plt.figure(figsize=(10, 10))
+plt.axis("off")
+plt.imshow(np.transpose(vutils.make_grid(gen_data, nrow=10, padding=2, normalize=True), (1,2,0)))
+plt.savefig("Epoch_%d_{}".format(params['dataset']) %(params['num_epochs']))
+
+# Save network weights.
+torch.save({
+    'netG' : netG.state_dict(),
+    'discriminator' : discriminator.state_dict(),
+    'classifier' : classifier.state_dict(),
+    'netD' : netD.state_dict(),
+    'netQ' : netQ.state_dict(),
+    'netC' : netC.state_dict(),
+    'optimD' : optimD.state_dict(),
+    'optimG' : optimG.state_dict(),
+    'params' : params
+    }, 'checkpoint/model_final_fringe_{}'.format(params['dataset']))
+
+
+# Plot the training losses.
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses,label="G")
+plt.plot(D_losses,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.savefig("Loss Curve {}".format(params['dataset']))
+
+# Animation showing the improvements of the generator.
+fig = plt.figure(figsize=(10,10))
+plt.axis("off")
+ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+anim = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+anim.save('infoGAN_{}.gif'.format(params['dataset']), dpi=80, writer='imagemagick')
+plt.show()
