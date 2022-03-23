a762b1dd98607596426a94f5a7e34c3fc19ec30c
Suraj Menon <sjmickiemouse@gmail.com>
2022-03-23 09:39:26 -0700

    Adding vicreg_eval file.

---
+++ vicreg-main/vicreg_eval.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import argparse
+import json
+import os
+import random
+import signal
+import sys
+import time
+import urllib
+
+from torch import nn, optim
+from torchvision import datasets, transforms
+import torch
+
+import resnet
+
