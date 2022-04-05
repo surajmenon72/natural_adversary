324234afe100556d21c3b25c4783efbebe1efff0
Suraj Menon <sjmickiemouse@gmail.com>
2022-04-04 20:52:46 -0700

    How is the entropy if we don't use backprop?

--- InfoGAN-PyTorch-master/mnist_generate_fringe.py
+++ InfoGAN-PyTorch-master/mnist_generate_fringe.py
@@ -34,7 +34,7 @@ print(netG)
 netG.eval()
 
 start = 0
-stop = 1
+stop = 3
 #c = np.linspace(-2, 2, 10).reshape(1, -1)
 c = np.linspace(start, stop, 10).reshape(1, -1)
 c = np.repeat(c, 10, 0).reshape(-1, 1)
@@ -57,7 +57,7 @@ reduction_factor = 1
 #c2 = torch.zeros((10, 100, 1, 1), device=device)
 c2 = torch.zeros((1, 100, 1, 1), device=device)
 #primary
-#c2[c_index] = c[:, :, 0]*reduction_factor
+c2[c_index] = c[:, :, 0]*reduction_factor
 #secondary
 #c2[c_index2] = (c[:, :, 0])/reduction_factor
 c2 = c2.permute(1, 0, 2, 3)
