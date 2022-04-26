54ee3809b9442ebb0f5669c8b189a12d432c3ccb
Suraj Menon <sjmickiemouse@gmail.com>
2022-04-26 12:11:59 -0700

    Can we not clip wweights?

--- InfoGAN-PyTorch-master/train_fringe5.py
+++ InfoGAN-PyTorch-master/train_fringe5.py
@@ -342,12 +342,12 @@ for epoch in range(params['num_epochs']):
         else:
             D_loss = torch.zeros(1)
 
-        # nn.utils.clip_grad_value_(discriminator.parameters(), clip_value_1)
-        # nn.utils.clip_grad_value_(netD.parameters(), clip_value_1)
+        nn.utils.clip_grad_value_(discriminator.parameters(), clip_value_1)
+        nn.utils.clip_grad_value_(netD.parameters(), clip_value_1)
         optimD.step()
         #need to clip WGAN for Lipshitz
-        clip_module_weights(discriminator, min_v=-.01, max_v=.01)
-        clip_module_weights(netD, min_v=-.01, max_v=.01)
+        # clip_module_weights(discriminator, min_v=-.01, max_v=.01)
+        # clip_module_weights(netD, min_v=-.01, max_v=.01)
 
         # stretcher.train()
         # netH.train()
