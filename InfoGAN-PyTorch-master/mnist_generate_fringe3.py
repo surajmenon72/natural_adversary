151b09b55e5caa48349778fd07d650d8d9d70b5f
Suraj Menon <sjmickiemouse@gmail.com>
2022-06-28 15:07:47 -0700

    Gonna train for the full continuous spectrum for now.

--- InfoGAN-PyTorch-master/mnist_generate_fringe3.py
+++ InfoGAN-PyTorch-master/mnist_generate_fringe3.py
@@ -34,8 +34,8 @@ print(netG)
 
 netG.eval()
 
-start = .599
-stop = .601
+# start = .599
+# stop = .601
 #c = np.linspace(-2, 2, 10).reshape(1, -1)
 c = np.linspace(start, stop, 10).reshape(1, -1)
 c = np.repeat(c, 10, 0).reshape(-1, 1)
