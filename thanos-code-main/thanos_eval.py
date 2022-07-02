3deb134284a0973436a160eaf6151e89b60cb05f
Suraj Menon <sjmickiemouse@gmail.com>
2022-07-02 11:31:04 -0700

    Leave out the last batch.

--- thanos-code-main/thanos_eval.py
+++ thanos-code-main/thanos_eval.py
@@ -386,7 +386,7 @@ def main_worker(args):
     #Set knn,works only if targets are about evenly distributed in training set
     model.eval()
     print (len(train_loader))
-    batches_for_knn = len(train_loader)
+    batches_for_knn = len(train_loader)-1
     exp_dir = './models/knn.pth'
     train_knn = 'True'
     knn_e = torch.zeros((batches_for_knn*batch_size, embedding_size))
