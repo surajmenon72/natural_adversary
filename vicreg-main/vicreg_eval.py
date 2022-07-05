f7f9442b5dd3f3bea524b769aef562a7748d54b6
Suraj Menon <sjmickiemouse@gmail.com>
2022-07-05 07:45:13 -0700

    Porting changes to vicreg.

--- vicreg-main/vicreg_eval.py
+++ vicreg-main/vicreg_eval.py
@@ -14,6 +14,7 @@ import torch
 
 import resnet
 import main_vicreg_mnist 
+import copy
 
 def get_arguments():
     parser = argparse.ArgumentParser(
@@ -317,8 +318,8 @@ def main_worker(args):
             start_index = i*batch_size
             end_index = (i+1)*batch_size
 
-            knn_e[start_index:end_index, :] = output[:, :]
-            knn_t[start_index:end_index] = target[:]
+            knn_e[start_index:end_index, :] = copy.deepcopy(output[:, :])
+            knn_t[start_index:end_index] = copy.deepcopy(target[:])
 
             if (i == (batches_for_knn-1)):
                 break
@@ -330,8 +331,8 @@ def main_worker(args):
         torch.save(state, args.exp_dir / "knn.pth")
     else:
         knn_dict = torch.load(args.exp_dir / "knn.pth")
-        knn_e = knn_dict["knn_e"]
-        knn_t = knn_dict["knn_t"]
+        knn_e = copy.deepcopy(knn_dict["knn_e"])
+        knn_t = copy.deepcopy(knn_dict["knn_t"])
         print ('KNN loaded')
 
     def calculate_fuzzy_k_means(model_output, k_means):
@@ -391,6 +392,39 @@ def main_worker(args):
 
         distances = torch.cdist(model_output_r, knn_e_r, p=2) #verified this works
         distances = distances.view((b_size, knn_size))
+
+        knn_guesses = torch.zeros((b_size, num_classes))
+        for i in range(b_size):
+            d = distances[i, :]
+            d_s, d_s_i = torch.sort(d)
+            max_val = torch.max(d_s)
+            for j in range(k):
+                knn_guesses[i, int(knn_t[d_s_i[j]])] += max_val - d_s[j]
+
+            #knn_guesses[i, :] /= torch.max(knn_guesses[i, :])
+
+        #print (knn_guesses[0])
+
+        #sm_knn = torch.nn.functional.softmax(knn_guesses, dim=1)
+
+        #sm_knn = knn_guesses/torch.sum(knn_guesses, dim=1)
+        sm_knn = knn_guesses/(torch.sum(knn_guesses, dim=1).view((knn_guesses.shape[0], 1)))
+
+        return sm_knn
+
+    def calculate_fuzzy_knn_eff(model_output, knn_e, knn_t, k=100, num_classes=10):
+        b_size = model_output.shape[0]
+        e_size = model_output.shape[1]
+        knn_size = knn_e.shape[0]
+
+        distances = torch.zeros((b_size, knn_size))
+        knn_e_r = knn_e.view((1, knn_size, e_size))
+        for b in range(b_size):
+            output_chunk = model_output[b, :]
+            output_chunk_r = output_chunk.view((1, 1, e_size))
+            distance_b = torch.cdist(output_chunk_r, knn_e_r, p=2)
+            distance_b = distance_b.view((knn_size))
+            distances[b, :] = distance_b[:]
 
         knn_guesses = torch.zeros((b_size, num_classes))
         for i in range(b_size):
