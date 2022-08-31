af7a4ebb2b9d7e1f0cdbc08fda11624e7c5799e3
Suraj Menon <sjmickiemouse@gmail.com>
2022-08-31 12:10:50 -0700

    Need these definitions.

--- InfoGAN-PyTorch-master/dataloader.py
+++ InfoGAN-PyTorch-master/dataloader.py
@@ -6,6 +6,28 @@ from torchvision.transforms import InterpolationMode
 # Directory containing the data.
 root = 'data/'
 
+class GaussianBlur(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img):
+        if np.random.rand() < self.p:
+            sigma = np.random.rand() * 1.9 + 0.1
+            return img.filter(ImageFilter.GaussianBlur(sigma))
+        else:
+            return img
+
+
+class Solarization(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img):
+        if np.random.rand() < self.p:
+            return ImageOps.solarize(img)
+        else:
+            return img
+
 def get_data(dataset, batch_size, train_test='train', use_3_channel=False):
 
     # Get MNIST dataset.
