4ca13cf68de35356cdb47740c127b876d9d9b736
Suraj Menon <sjmickiemouse@gmail.com>
2022-08-31 22:07:41 -0700

    test.

--- InfoGAN-PyTorch-master/models/mnist_model_wtsmooth2.py
+++ InfoGAN-PyTorch-master/models/mnist_model_wtsmooth2.py
@@ -560,7 +560,7 @@ class Discriminator_CIFAR_Identity(nn.Module):
             #nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            #nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
         )
 
     def forward(self, input):
