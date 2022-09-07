42d91e140bc59b44ce0147a668163f1c0aa9e406
Suraj Menon <sjmickiemouse@gmail.com>
2022-09-06 17:28:55 -0700

    Adding in shuffle to test.

--- InfoGAN-PyTorch-master/train_fringe10_identity.py
+++ InfoGAN-PyTorch-master/train_fringe10_identity.py
@@ -408,20 +408,20 @@ for epoch in range(params['num_epochs']):
             loss_real.backward()
 
             #Shuffled data
-            # label.fill_(fake_label)
-            # shuffled_data = torch.zeros((b_size, channels, d0, d1), device=device)
-            # shuffled_data[0] = real_data[-1]
-            # shuffled_data[1:] = real_data[:b_size-1]
+            label.fill_(fake_label)
+            shuffled_data = torch.zeros((b_size, channels, d0, d1), device=device)
+            shuffled_data[0] = real_data[-1]
+            shuffled_data[1:] = real_data[:b_size-1]
 
-            # #shuffled_data_double = torch.cat([shuffled_data, real_data], dim=1)
-            # real_output = discriminator(real_data)
-            # shuffled_output = discriminator(shuffled_data)
-            # shuffled_output_double = torch.cat([shuffled_output, real_output], dim=1)
-            # probs_fake_s = netD(torch.squeeze(shuffled_output_double)).view(-1)
-            # label = label.to(torch.float32)
-            # loss_shuffle = criterionD(probs_fake_s, label)
-            # #calculate grad
-            # loss_shuffle.backward()
+            #shuffled_data_double = torch.cat([shuffled_data, real_data], dim=1)
+            real_output = discriminator(real_data)
+            shuffled_output = discriminator(shuffled_data)
+            shuffled_output_double = torch.cat([shuffled_output, real_output], dim=1)
+            probs_fake_s = netD(torch.squeeze(shuffled_output_double)).view(-1)
+            label = label.to(torch.float32)
+            loss_shuffle = criterionD(probs_fake_s, label)
+            #calculate grad
+            loss_shuffle.backward()
 
             # Generate fake image batch with G
             # fake_data = netG(z_noise)
@@ -446,9 +446,9 @@ for epoch in range(params['num_epochs']):
             #calculate grad
             loss_fake.backward()
 
-            D_loss = loss_real + loss_fake
+            #D_loss = loss_real + loss_fake
             #D_loss = loss_real + loss_shuffle
-            #D_loss = loss_real + loss_shuffle + loss_fake
+            D_loss = loss_real + loss_shuffle + loss_fake
             #D_loss.backward()
         else:
             D_loss = torch.zeros(1)
