5c75869b272ab787d42af34003064866d62373d5
Suraj Menon <sjmickiemouse@gmail.com>
2022-09-14 16:28:30 -0700

    Continuing training.

--- InfoGAN-PyTorch-master/train_fringe10_identity_cifar.py
+++ InfoGAN-PyTorch-master/train_fringe10_identity_cifar.py
@@ -21,7 +21,7 @@ from config import params
 from models.mnist_model_wtsmooth2 import Generator, Generator_Resnet, Generator_CIFAR, Discriminator, Discriminator_Resnet, Discriminator_CIFAR, Discriminator_CIFAR_Identity, DHead, DHead_KL, DHead_Resnet, DHead_CIFAR_Identity, QHead, Encoder, ResnetEncoder, ResNet18Dec, CHead, Stretcher, HHead
 
 # Set random seed for reproducibility.
-seed = 1132
+seed = 1133
 random.seed(seed)
 torch.manual_seed(seed)
 print("Random Seed: ", seed)
@@ -45,7 +45,7 @@ extra_transforms =  transforms.Compose([
                         ),
                     ])
 
-load_model = False
+load_model = True
 load_classifier = False
 
 use_base_resnet = 'resnet'
