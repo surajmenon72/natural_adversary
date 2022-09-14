3cde3c9a524c1e1d256bf6d71b4dc6a2d8ccb3fc
Suraj Menon <sjmickiemouse@gmail.com>
2022-09-13 17:12:48 -0700

    dbg.

--- InfoGAN-PyTorch-master/train_fringe10_identity.py
+++ InfoGAN-PyTorch-master/train_fringe10_identity.py
@@ -452,7 +452,7 @@ for epoch in range(params['num_epochs']):
             print (idx.shape)
             print (fake_output_double.shape)
             idx_t = torch.tensor(idx).to(device)
-            idx_r = idx_t.view((b_size, 1))
+            idx_r = idx_t.view((b_size, 1, 1, 1))
             fake_output_double = torch.cat([fake_output_double, idx_r], dim=1)
             print (fake_output_double.shape)
             exit()
