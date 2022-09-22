dffd77380523a1af55a595c4d9e06017f5b7d23c
Suraj Menon <sjmickiemouse@gmail.com>
2022-09-21 17:03:16 -0700

    saturation no, try hue.

--- InfoGAN-PyTorch-master/train_fringe10_identity.py
+++ InfoGAN-PyTorch-master/train_fringe10_identity.py
@@ -63,7 +63,7 @@ extra_transforms =  transforms.Compose([
                         transforms.RandomApply(
                             [
                                 transforms.ColorJitter(
-                                    brightness=0.0, contrast=0.0, saturation=0.2, hue=0.0
+                                    brightness=0.0, contrast=0.0, saturation=0.0, hue=0.1
                                 )
                             ],
                             p=0.9,
