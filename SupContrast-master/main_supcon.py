6691938c80b3fb5ab679036d16ea4ada57821d41
Suraj Menon <sjmickiemouse@gmail.com>
2022-09-27 10:54:49 -0700

    Ok, now testing this out.

--- SupContrast-master/main_supcon.py
+++ SupContrast-master/main_supcon.py
@@ -217,8 +217,13 @@ def train(train_loader, model, criterion, optimizer, epoch, opt):
 
         # compute loss
         features = model(images)
+        print ('Pre shape:')
+        print (features.shape)
         f1, f2 = torch.split(features, [bsz, bsz], dim=0)
         features = torch.cat([f1.unsqueeze(1), f2.unsqueeze(1)], dim=1)
+        print ('Post shape:')
+        print (features.shape)
+        exit()
         if opt.method == 'SupCon':
             loss = criterion(features, labels)
         elif opt.method == 'SimCLR':
