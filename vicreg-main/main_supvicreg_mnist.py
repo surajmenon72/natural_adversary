83200b7652a5448d91be084747cedce99e6b5411
Suraj Menon <sjmickiemouse@gmail.com>
2022-09-28 10:26:17 -0700

    Combining Vicreg and Supcon.

--- vicreg-main/main_supvicreg_mnist.py
+++ vicreg-main/main_supvicreg_mnist.py
@@ -29,6 +29,95 @@ import resnet
 import numpy as np
 import matplotlib.pyplot as plt
 
+class SupConLoss(nn.Module):
+    """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
+    It also supports the unsupervised contrastive loss in SimCLR"""
+    def __init__(self, temperature=0.07, contrast_mode='all',
+                 base_temperature=0.07):
+        super(SupConLoss, self).__init__()
+        self.temperature = temperature
+        self.contrast_mode = contrast_mode
+        self.base_temperature = base_temperature
+
+    def forward(self, features, labels=None, mask=None):
+        """Compute loss for model. If both `labels` and `mask` are None,
+        it degenerates to SimCLR unsupervised loss:
+        https://arxiv.org/pdf/2002.05709.pdf
+
+        Args:
+            features: hidden vector of shape [bsz, n_views, ...].
+            labels: ground truth of shape [bsz].
+            mask: contrastive mask of shape [bsz, bsz], mask_{i,j}=1 if sample j
+                has the same class as sample i. Can be asymmetric.
+        Returns:
+            A loss scalar.
+        """
+        device = (torch.device('cuda')
+                  if features.is_cuda
+                  else torch.device('cpu'))
+
+        if len(features.shape) < 3:
+            raise ValueError('`features` needs to be [bsz, n_views, ...],'
+                             'at least 3 dimensions are required')
+        if len(features.shape) > 3:
+            features = features.view(features.shape[0], features.shape[1], -1)
+
+        batch_size = features.shape[0]
+        if labels is not None and mask is not None:
+            raise ValueError('Cannot define both `labels` and `mask`')
+        elif labels is None and mask is None:
+            mask = torch.eye(batch_size, dtype=torch.float32).to(device)
+        elif labels is not None:
+            labels = labels.contiguous().view(-1, 1)
+            if labels.shape[0] != batch_size:
+                raise ValueError('Num of labels does not match num of features')
+            mask = torch.eq(labels, labels.T).float().to(device)
+        else:
+            mask = mask.float().to(device)
+
+        contrast_count = features.shape[1]
+        contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)
+        if self.contrast_mode == 'one':
+            anchor_feature = features[:, 0]
+            anchor_count = 1
+        elif self.contrast_mode == 'all':
+            anchor_feature = contrast_feature
+            anchor_count = contrast_count
+        else:
+            raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
+
+        # compute logits
+        anchor_dot_contrast = torch.div(
+            torch.matmul(anchor_feature, contrast_feature.T),
+            self.temperature)
+        # for numerical stability
+        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
+        logits = anchor_dot_contrast - logits_max.detach()
+
+        # tile mask
+        mask = mask.repeat(anchor_count, contrast_count)
+        # mask-out self-contrast cases
+        logits_mask = torch.scatter(
+            torch.ones_like(mask),
+            1,
+            torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
+            0
+        )
+        mask = mask * logits_mask
+
+        # compute log_prob
+        exp_logits = torch.exp(logits) * logits_mask
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+        # compute mean of log-likelihood over positive
+        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+
+        # loss
+        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+        loss = loss.view(anchor_count, batch_size).mean()
+
+        return loss
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(description="Pretrain a resnet model with VICReg", add_help=False)
@@ -187,7 +276,9 @@ def main(args):
         shuffle=True
     )
 
-    model = VICReg(args).to(device)
+    model = SupVICReg(args).to(device)
+
+    sup_criterion = SupConLoss(temperature=0.07)
 
     #to extract backbone if necessary
     # ckpt = torch.load(args.exp_dir / "model.pth", map_location="cpu")
@@ -221,18 +312,22 @@ def main(args):
     #start_time = last_logging = time.time()
     start_time = time.time()
     #scaler = torch.cuda.amp.GradScaler()
+    alpha = 0.5
     for epoch in range(start_epoch, args.epochs):
         #sampler.set_epoch(epoch)
         print ('Epoch')
         print (epoch)
         epoch_start_time = time.time()
         #scheduler.step()
-        for step, ((x, y), _) in enumerate(loader):
+        for step, ((x, y), labels) in enumerate(loader):
             # x = x.cuda(gpu, non_blocking=True)
             # y = y.cuda(gpu, non_blocking=True)
 
             x = x.to(device)
             y = y.to(device)
+            bsz = labels.shape[0]
+
+            xy = torch.cat([x, y], dim=0)
 
             #lr = adjust_learning_rate(args, optimizer, loader, step)
 
@@ -244,11 +339,24 @@ def main(args):
             # scaler.step(optimizer)
             # scaler.update()
 
-            loss = model.forward(x, y)
+            #loss = model.forward(x, y)
+            xy = model.forward_only(xy)
+            x, y = torch.split(xy, [bsz, bsz], dim=0)
+
+            vicreg_loss = model.loss_only(x, y)
+
+            xy_features = torch.cat([x.unsqueeze(1), y.unsqueeze(1)], dim=1)
+            supcon_loss = sup_criterion(features, labels)
+
 
             if ((step % 50) == 0):
-                print ('Current Loss')
-                print (loss)
+                print ('Current Vicreg Loss')
+                print (vicreg_loss)
+                print ('Current Supcon Loss')
+                print (supcon_loss)
+
+            loss = alpha*vicreg_loss + (1-alpha)*supcon_loss
+            
             loss.backward()
             optimizer.step()
 
@@ -297,7 +405,7 @@ def adjust_learning_rate(args, optimizer, loader, step):
     return lr
 
 
-class VICReg(nn.Module):
+class SupVICReg(nn.Module):
     def __init__(self, args):
         super().__init__()
         self.args = args
@@ -322,6 +430,37 @@ class VICReg(nn.Module):
         x = self.projector(self.backbone(x))
         y = self.projector(self.backbone(y))
 
+        repr_loss = F.mse_loss(x, y)
+
+        #x = torch.cat(FullGatherLayer.apply(x), dim=0)
+        #y = torch.cat(FullGatherLayer.apply(y), dim=0)
+
+        x = x - x.mean(dim=0)
+        y = y - y.mean(dim=0)
+
+        std_x = torch.sqrt(x.var(dim=0) + 0.0001)
+        std_y = torch.sqrt(y.var(dim=0) + 0.0001)
+        std_loss = torch.mean(F.relu(1 - std_x)) / 2 + torch.mean(F.relu(1 - std_y)) / 2
+
+        cov_x = (x.T @ x) / (self.args.batch_size - 1)
+        cov_y = (y.T @ y) / (self.args.batch_size - 1)
+        cov_loss = off_diagonal(cov_x).pow_(2).sum().div(
+            self.num_features
+        ) + off_diagonal(cov_y).pow_(2).sum().div(self.num_features)
+
+        loss = (
+            self.args.sim_coeff * repr_loss
+            + self.args.std_coeff * std_loss
+            + self.args.cov_coeff * cov_loss
+        )
+        return loss
+
+    def forward_only(self, x):
+        x = self.projector(self.backbone(x))
+
+        return x
+
+    def loss_only(self, x, y):
         repr_loss = F.mse_loss(x, y)
 
         #x = torch.cat(FullGatherLayer.apply(x), dim=0)
