5c1ed5d0a467c18d6e772ad274af6e56962f82cb
Suraj Menon <sjmickiemouse@gmail.com>
2022-09-28 16:10:36 -0700

    Ok, is random trainable now?

--- vicreg-main/main_supvicreg_mnist_random.py
+++ vicreg-main/main_supvicreg_mnist_random.py
@@ -29,114 +29,7 @@ import resnet
 import numpy as np
 import matplotlib.pyplot as plt
 
-class SupConLoss(nn.Module):
-    """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
-    It also supports the unsupervised contrastive loss in SimCLR"""
-    def __init__(self, temperature=0.07, contrast_mode='all',
-                 base_temperature=0.07):
-        super(SupConLoss, self).__init__()
-        self.temperature = temperature
-        self.contrast_mode = contrast_mode
-        self.base_temperature = base_temperature
-
-    def forward(self, features, labels=None, mask=None):
-        """Compute loss for model. If both `labels` and `mask` are None,
-        it degenerates to SimCLR unsupervised loss:
-        https://arxiv.org/pdf/2002.05709.pdf
-
-        Args:
-            features: hidden vector of shape [bsz, n_views, ...].
-            labels: ground truth of shape [bsz].
-            mask: contrastive mask of shape [bsz, bsz], mask_{i,j}=1 if sample j
-                has the same class as sample i. Can be asymmetric.
-        Returns:
-            A loss scalar.
-        """
-        device = (torch.device('cuda')
-                  if features.is_cuda
-                  else torch.device('cpu'))
-
-        if len(features.shape) < 3:
-            raise ValueError('`features` needs to be [bsz, n_views, ...],'
-                             'at least 3 dimensions are required')
-        if len(features.shape) > 3:
-            features = features.view(features.shape[0], features.shape[1], -1)
-
-        batch_size = features.shape[0]
-        if labels is not None and mask is not None:
-            raise ValueError('Cannot define both `labels` and `mask`')
-        elif labels is None and mask is None:
-            mask = torch.eye(batch_size, dtype=torch.float32).to(device)
-        elif labels is not None:
-            labels = labels.contiguous().view(-1, 1)
-            if labels.shape[0] != batch_size:
-                raise ValueError('Num of labels does not match num of features')
-            mask = torch.eq(labels, labels.T).float().to(device)
-        else:
-            mask = mask.float().to(device)
-
-        contrast_count = features.shape[1]
-        contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)
-        if self.contrast_mode == 'one':
-            anchor_feature = features[:, 0]
-            anchor_count = 1
-        elif self.contrast_mode == 'all':
-            anchor_feature = contrast_feature
-            anchor_count = contrast_count
-        else:
-            raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
-
-        # compute logits
-        anchor_dot_contrast = torch.div(
-            torch.matmul(anchor_feature, contrast_feature.T),
-            self.temperature)
-        # for numerical stability
-        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
-        logits = anchor_dot_contrast - logits_max.detach()
-
-        # tile mask
-        mask = mask.repeat(anchor_count, contrast_count)
-        # mask-out self-contrast cases
-        logits_mask = torch.scatter(
-            torch.ones_like(mask),
-            1,
-            torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
-            0
-        )
-        mask = mask * logits_mask
-
-        print ('logits')
-        print (torch.sum(logits))
-        print ('mask')
-        print (torch.sum(mask))
-
-        # compute log_prob
-        exp_logits = torch.exp(logits) * logits_mask
-        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
-
-        print ('exp logits')
-        print (torch.sum(exp_logits))
-        print ('log prob')
-        print (torch.sum(log_prob))
-
-        print ('mask sum')
-        print (mask.sum(1))
-        print (mask.sum(1).shape)
-
-        # compute mean of log-likelihood over positive
-        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
-
-        print ('mean log prob')
-        print (torch.sum(mean_log_prob_pos))
-
-        # loss
-        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
-        loss = loss.view(anchor_count, batch_size).mean()
-
-        print ('loss')
-        print (loss)
-
-        return loss
+from augmentations import SupConLoss
 
 
 def get_arguments():
@@ -333,6 +226,7 @@ def main(args):
     start_time = time.time()
     #scaler = torch.cuda.amp.GradScaler()
     alpha = 0.5
+    r = 0.1
     for epoch in range(start_epoch, args.epochs):
         #sampler.set_epoch(epoch)
         print ('Epoch')
@@ -378,7 +272,7 @@ def main(args):
             #supcon_loss = torch.zeros(1)
 
             #mask = torch.zeros((bsz, bsz))
-            p = 2
+            p = 2 #for now not dealing w/ div by 0s, forcing every example to pick someone random to be attracted to
             mask_1 = torch.rand(bsz, dtype=torch.float32).to(device)
             mask_1 = (mask_1 < p)
             mask_2 = torch.randint(low=0, high=bsz-1, size=(bsz,)).to(device)
@@ -387,8 +281,6 @@ def main(args):
             onehot_mask[:, 0] *= 0 #no one is attracting to 0, shouldnt matter
 
             random_loss = sup_criterion(xy_features_s, labels=None, mask=onehot_mask)
-            print (random_loss)
-            exit()
 
             if ((step % 50) == 0):
                 print ('Current Vicreg Loss')
@@ -396,7 +288,7 @@ def main(args):
                 print ('Current Supcon Loss')
                 print (supcon_loss)
 
-            loss = alpha*vicreg_loss + (1-alpha)*supcon_loss
+            loss = alpha*vicreg_loss + (1-alpha)*supcon_loss + r*random_loss
             
             loss.backward()
             optimizer.step()
