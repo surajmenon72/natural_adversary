0bebe0bd47973a2b1507d4a26a861605a72a41be
Suraj Menon <sjmickiemouse@gmail.com>
2022-09-29 10:15:29 -0700

    Generate knn.

--- InfoGAN-PyTorch-master/classifier_eval.py
+++ InfoGAN-PyTorch-master/classifier_eval.py
@@ -385,7 +385,8 @@ def main_worker(args):
                 missing_keys, unexpected_keys = backbone.load_state_dict(loaded, strict=False)
             else:
                 #ckpt = './checkpoints/vicreg_backbone_base_60.pth'
-                ckpt = './checkpoints/vicreg_backbone_base_fashion_60.pth'
+                #ckpt = './checkpoints/vicreg_backbone_base_fashion_60.pth'
+                ckpt = './checkpoints/supvic_backbone_base_fashion_60.pth'
                 loaded = torch.load(ckpt, map_location=torch.device('cpu'))
                 missing_keys, unexpected_keys = backbone.load_state_dict(loaded, strict=False)
 
